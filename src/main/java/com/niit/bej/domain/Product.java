dde077f4cdee0e7123c70f4eb669d2f234f31f3b
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 14:43:52 +0530

    define Constructor for Product

--- src/main/java/com/niit/bej/domain/Product.java
+++ src/main/java/com/niit/bej/domain/Product.java
@@ -13,4 +13,12 @@ public class Product {
 
     private String description;
 
+    public Product(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Product() {
+    }
 }
