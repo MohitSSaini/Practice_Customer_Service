d1c31f28cb84457d9d5c6b3d10d2b3f871da2404
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 16:52:47 +0530

    define addCustomer method and annotation @PostMapping for CustomerController

--- src/main/java/com/niit/bej/controller/CustomerController.java
+++ src/main/java/com/niit/bej/controller/CustomerController.java
@@ -5,8 +5,14 @@
  */
 package com.niit.bej.controller;
 
+import com.niit.bej.domain.Customer;
+import com.niit.bej.exception.CustomerAlreadyExist;
 import com.niit.bej.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +23,16 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
+
+    @PostMapping("/addCustomer")
+    public ResponseEntity<?> addCustomer(@RequestBody Customer customer) {
+        try {
+            Customer insertedcustomer = customerService.addCustomer(customer);
+            return new ResponseEntity<>(insertedcustomer, HttpStatus.CREATED);
+        } catch (CustomerAlreadyExist exception) {
+            return new ResponseEntity<>(exception, HttpStatus.CONFLICT);
+        }
+    }
+
+
 }
