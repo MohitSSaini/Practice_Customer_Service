ba0248eeed0b5c122b2f80be353bf2ff0e4cff8b
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 17:02:30 +0530

    define getCustomer method and annotation @GetMapping for CustomerController

--- src/main/java/com/niit/bej/controller/CustomerController.java
+++ src/main/java/com/niit/bej/controller/CustomerController.java
@@ -11,9 +11,12 @@ import com.niit.bej.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
@@ -34,5 +37,13 @@ public class CustomerController {
         }
     }
 
-
+    @GetMapping("/getCustomer")
+    public ResponseEntity<?> getCustomer() {
+        try {
+            List<Customer> customerList = customerService.getAllCustomer();
+            return new ResponseEntity<>(customerList, HttpStatus.FOUND);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(exception, HttpStatus.CONFLICT);
+        }
+    }
 }
