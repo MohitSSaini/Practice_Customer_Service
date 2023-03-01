9c3292c1f5d08221baec92e2df3f054cd2322691
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 15:29:22 +0530

    define list method and annotate @Query for CustomerRepository

--- src/main/java/com/niit/bej/repository/CustomerRepository.java
+++ src/main/java/com/niit/bej/repository/CustomerRepository.java
@@ -7,6 +7,12 @@ package com.niit.bej.repository;
 
 import com.niit.bej.domain.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface CustomerRepository extends MongoRepository<Customer, Integer> {
+    //    Optional<Customer>findById(int id);
+    @Query("{'product.name': ?0}")
+    List<Customer> fetchAllCustomerByProductName(String name);
 }
