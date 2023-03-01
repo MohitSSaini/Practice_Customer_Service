6cef6ad7da7afbf965e1159d793e0ebf3286c802
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 14:44:13 +0530

    define getter and setter method for Product

--- src/main/java/com/niit/bej/domain/Product.java
+++ src/main/java/com/niit/bej/domain/Product.java
@@ -21,4 +21,28 @@ public class Product {
 
     public Product() {
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
