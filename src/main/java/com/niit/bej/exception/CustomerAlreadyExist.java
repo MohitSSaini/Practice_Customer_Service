573b1ab20fcdb93240965cef0c184c1ced7e5b1c
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 16:40:05 +0530

    define Exception method for CustomerAlreadyExist

---
+++ src/main/java/com/niit/bej/exception/CustomerAlreadyExist.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 01-03-2023
+ * Created With: IntelliJ IDEA Ultimate
+ */
+package com.niit.bej.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Customer Data Already Present")
+public class CustomerAlreadyExist extends Exception {
+}
