a424736e0eacf15d5c4543b1b09cbfdc9cc956fa
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 15:45:55 +0530

    define CustomerRepository and Annotation @Service and @Autowired for CustomerServiceImpl.java

--- src/main/java/com/niit/bej/service/CustomerServiceImpl.java
+++ src/main/java/com/niit/bej/service/CustomerServiceImpl.java
@@ -5,5 +5,39 @@
  */
 package com.niit.bej.service;
 
-public class CustomerServiceImpl {
+import com.niit.bej.domain.Customer;
+import com.niit.bej.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Customer addCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAllCustomer() {
+        return null;
+    }
+
+    @Override
+    public boolean deleteCustomerById(int id) {
+        return false;
+    }
+
+    @Override
+    public List<Customer> getAllCustomerByProductName(String name) {
+        return null;
+    }
 }
