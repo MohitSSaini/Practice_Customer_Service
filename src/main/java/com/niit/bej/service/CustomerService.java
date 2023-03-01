372238606af50a41b6002789620e8c63e1cdb440
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 15:42:31 +0530

    define method for CustomerService.java

--- src/main/java/com/niit/bej/service/CustomerService.java
+++ src/main/java/com/niit/bej/service/CustomerService.java
@@ -1,4 +1,15 @@
 package com.niit.bej.service;
 
+import com.niit.bej.domain.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
+    Customer addCustomer(Customer customer);
+
+    List<Customer> getAllCustomer();
+
+    boolean deleteCustomerById(int id);
+
+    List<Customer> getAllCustomerByProductName(String name);
 }
