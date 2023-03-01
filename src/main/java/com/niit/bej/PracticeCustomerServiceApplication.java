57f11ffa978ebf3c6fd4268699cc48fe447a78ae
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 14:33:07 +0530

    initial commit

---
+++ src/main/java/com/niit/bej/PracticeCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.niit.bej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeCustomerServiceApplication.class, args);
+	}
+
+}
