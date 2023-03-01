3dc0f2514ba18ab6bfb3b2de944a159f678cbfb4
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 16:45:34 +0530

    define annotation @RestController and @Autowired for CustomerController

--- src/main/java/com/niit/bej/controller/CustomerController.java
+++ src/main/java/com/niit/bej/controller/CustomerController.java
@@ -6,10 +6,14 @@
 package com.niit.bej.controller;
 
 import com.niit.bej.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CustomerController {
     public final CustomerService customerService;
 
+    @Autowired
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
