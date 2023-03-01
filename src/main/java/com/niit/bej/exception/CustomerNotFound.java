1002a3a320944e46273fb72c34a026764a8c47f5
Mohit__Saini <mohit.saini09868@gmail.com>
2023-03-01 16:40:17 +0530

    define Exception method for CustomerNotFound

---
+++ src/main/java/com/niit/bej/exception/CustomerNotFound.java
@@ -0,0 +1,14 @@
+/*
+ * Author Name: Mohit Saini
+ * Date: 01-03-2023
+ * Created With: IntelliJ IDEA Ultimate
+ */
+package com.niit.bej.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "no matching customer found")
+
+public class CustomerNotFound extends Exception {
+}
